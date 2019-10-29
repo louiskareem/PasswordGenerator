06a07fa1aac139c3c4643cb8bce0f3a060bfdf17
louiskareem <louiskareemcocks@hotmail.com>
2019-10-29 14:03:29 +0100

    First commit. Simple password generator in c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.password_textbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // password_textbox
+            // 
+            this.password_textbox.Location = new System.Drawing.Point(265, 167);
+            this.password_textbox.Name = "password_textbox";
+            this.password_textbox.Size = new System.Drawing.Size(435, 22);
+            this.password_textbox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate password";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Random Password Generator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password_textbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox password_textbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // When button is click, execute the CreatRandomPassword method.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Display password in the textbox
+            password_textbox.Text = CreateRandomPassword();
+        }
+
+        // Method to create a random number
+        public int RandomNumber(int min, int max)
+        {
+            // Use the Random functin to create a random set of number 
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        // Method to create a random string 
+        public string RandomString(int size, bool lowerCase)
+        {
+            // Use the random and string builder functions 
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+
+            // char variable
+            char ch;
+
+            // Create a random set of number and convert them to characters then add it to the CH variable
+            for(int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            // If lowerCase is true then return the variable, if not then return string
+            if(lowerCase)
+                return builder.ToString().ToLower();
+           return builder.ToString();
+        }
+
+        // Method to create a random password
+        public string RandomPassword(int size = 0)
+        {
+            // Use the string builder function, then randomize numbers and string 
+            // and add them to the called functions and return the string
+            StringBuilder builder = new StringBuilder();
+            builder.Append(RandomString(4, true));
+            builder.Append(RandomNumber(1000, 9999));
+            builder.Append(RandomString(2, false));
+
+            return builder.ToString();
+            
+        }
+
+        // Method to create a random password with a length of 15 characters 
+        private static string CreateRandomPassword(int length = 15)
+        {
+            // Make a string of characters that are allowed to be used
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+
+            // Use the random function
+            Random random = new Random();
+
+            // Create an character array
+            char[] chars = new char[length];
+
+            // Put the valid characters in the characters array, then return the string 
+            for(int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+            return new string(chars);
+        }
+    }
+}
